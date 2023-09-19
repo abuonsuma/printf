cb5b73420ac5bf26a3b34d3e0be6ab5c5733e7dd
abuonsuma <abuonsuma@gmail.com>
2023-09-19 16:21:32 -0700

    It's a team this thing

--- main.h
+++ main.h
@@ -30,7 +30,6 @@ struct fmt
 	int (*fn)(va_list, char[], int, int, int, int);
 };
 
-
 /**
  * typedef struct fmt fmt_t - Struct op
  * @fmt: The format.
@@ -42,7 +41,7 @@ int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
 
-/****************** FUNCTIONS ******************/
+// Functions(Prototypes)
 
 // Funtions to print chars and strings
 int print_char(va_list types, char buffer[],
